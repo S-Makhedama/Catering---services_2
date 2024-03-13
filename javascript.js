8c9a8e0c84c44e3c7ed271541a0062008b90997e
Sopeza00 <158051431+Sopeza00@users.noreply.github.com>
2024-03-13 11:08:38 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 
+    <!-- JavaScript function to catering-->
+function showGallery(galleryId) {
+            const africanGallery = document.getElementById('africanGallery');
+            const modernGallery = document.getElementById('modernGallery');
+
+            if (galleryId === 'african-cusions') {
+                africanGallery.style.display = 'block';
+                modernGallery.style.display = 'none';
+            } else if (galleryId === 'modern-cusions') {
+                africanGallery.style.display = 'none';
+                modernGallery.style.display = 'block';
+            }
+        }
